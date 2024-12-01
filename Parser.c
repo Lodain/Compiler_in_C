cfc5ecab9d93fb34a99a94f11d6ba437886324ff
Danilo <danilospera@gmail.com>
2024-12-01 19:51:49 +0100

    Added the rule and the struct for tree

--- Parser.c
+++ Parser.c
@@ -9,8 +9,36 @@ enum Operators {
     ADD, SUB, MUL, DIV, MOD, POW, TERN, COMMA, LPAREN, RPAREN, I, DOLLAR
 };
 
+typedef enum {
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_FUNCTION,
+    TOKEN_PAREN_OPEN,
+    TOKEN_PAREN_CLOSE,
+    TOKEN_COMMA,
+    TOKEN_END
+} TokenType;
+
+// Define a token structure
+typedef struct {
+    TokenType type;
+    char value[32];
+} Token;
+
+typedef struct treeNode{
+    int data;
+    struct treeNode *left;
+    struct treeNode *right;
+} TreeNode;
+typedef TreeNode *TreeNodePtr;
+
+typedef struct {
+    char left;
+    char right[10];
+}Rule;
+
 // Precedence table
-char precedenceTable[NUM_OPERATORS][NUM_OPERATORS] = {
+static char precedenceTable[NUM_OPERATORS][NUM_OPERATORS] = {
     // ADD  SUB  MUL  DIV  MOD  POW  TERN COMMA LPAREN RPAREN  I   DOLLAR
     { '>', '>', '<', '<', '<', '<', '<', '>', '<', '>', '<', '>' }, // ADD
     { '>', '>', '<', '<', '<', '<', '<', '>', '<', '>', '<', '>' }, // SUB
@@ -26,3 +54,19 @@ char precedenceTable[NUM_OPERATORS][NUM_OPERATORS] = {
     { '<', '<', '<', '<', '<', '<', '<', '<', '<', ' ', '<', ' ' }  // DOLLAR
 };
 
+static Rule rules[10] = {
+    {'S', "i"},         //1: S -> i
+    {'S', "add A"},     //2: S -> add A
+    {'S', "sub A"},     //3: S -> sub A
+    {'S', "mul A"},     //4: S -> mul A
+    {'S', "div A"},     //5: S -> div A
+    {'S', "mod A"},     //6: S -> mod A
+    {'S', "pow A"},     //7: S -> pow A
+    {'S', "tern B"},    //8: S -> tern B
+    {'A', "(S,S)"},     //9: A -> (S,S)
+    {'B', "(S,S,S)"},   //10: B -> (S,S,S)
+};
+
+char* parse(Token* tokens, int numTokens) {
+
+}
